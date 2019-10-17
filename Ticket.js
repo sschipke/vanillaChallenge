bf9c1d923f30574ed44ba3a7e0047f7a9c26ebc0
Scott Schipke <swschipke@gmail.com>
2019-10-17 14:57:08 -0600

    Add Ticket class

---
+++ Ticket.js
@@ -0,0 +1,9 @@
+class Ticket {
+  constructor(title, theater, description, category) {
+    this.title= title;
+    this.theater = theater;
+    this.description = description;
+    this.category=category
+  }
+  
+}
---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+//query selectors
+const titleInput = document.getElementById('title');
+const theaterInput = document.getElementById('theater');
+const descriptionInput = document.getElementById('description');
+const categoryInput = document.getElementById('category-select');
+const addButton = document.getElementById('add-btn');
+const stage = document.querySelector('section')
+
+//event Listeners
+
+addButton.addEventListener('click', makeNewTicket)
+
+// Variables
+
+var tickets = [];
+
+//functions
+
+make
+
+
